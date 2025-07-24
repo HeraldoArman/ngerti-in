dea5c020a524f30a44a44a5cec8f04c03d04d601
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-07-24 18:02:39 +0700

    feat: add navbar and feature component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,13 @@
+import Navbar from "@/modules/landing/ui/navbar";
+import Features from "@/modules/landing/views/features";
 import Hero from "@/modules/landing/views/hero";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <Hero />
+      <Features />
     </div>
   );
 }
