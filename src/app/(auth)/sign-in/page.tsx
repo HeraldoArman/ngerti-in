9a54ea2ac04b52b1f4d32d0cc0a881fa692614fa
HeraldoArman <heraldoarman7@gmail.com>
2025-07-24 15:20:19 +0700

    banyak

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import SignInView from "@/modules/auth/sign-in-view";
 const Page = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
@@ -12,7 +13,7 @@ const Page = async () => {
   }
   return (
     <>
-      <div></div>
+      <SignInView />
     </>
   );
 };
