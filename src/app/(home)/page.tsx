2bbe8ed7a17b8ad57a3d1606928c7d0163741bdc
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-07-24 15:21:18 +0700

    feat: add image

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -10,21 +10,21 @@ const Hero = () => {
       <div className="absolute left-0 top-0 bottom-0 -z-10 w-full">
         <div className="absolute inset-0 h-full w-full bg-hero-background bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:40px_40px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_60%,transparent_100%)]"></div>
       </div>
-      
+
       <div className="text-center">
         <h1 className="text-4xl md:text-6xl md:leading-tight font-bold text-foreground max-w-lg md:max-w-2xl mx-auto">
           This is the main text
         </h1>
-        <p className="mt-4 text-foreground max-w-lg mx-auto">This is the longer secondary text that is very long</p>
+        <p className="mt-4 text-foreground max-w-lg mx-auto">
+          This is the longer secondary text that is very long
+        </p>
         <div className="mt-6 flex flex-col sm:flex-row items-center sm:gap-4 w-fit mx-auto">
           <Button variant={"secondary"} className="cursor-pointer">
-            Hello
+            Get Started!
           </Button>
         </div>
         <Image
-          src={
-            "https://images.unsplash.com/photo-1751647670942-3fe932006357?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1fHx8ZW58MHx8fHx8"
-          }
+          src={"./macbookpro.png"}
           width={384}
           height={340}
           quality={100}
