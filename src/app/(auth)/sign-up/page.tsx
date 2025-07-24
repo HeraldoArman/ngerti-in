d865025998752fe1fba5b27c005b5987992e1c82
HeraldoArman <heraldoarman7@gmail.com>
2025-07-24 14:39:50 +0700

    feat: db and better auth

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,21 @@
+// import SignUpView from "@/modules/auth/ui/views/sign-up-view";
+import React from "react";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+const Page = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!!session) {
+    redirect("/dashboard");
+  }
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+
+export default Page;
